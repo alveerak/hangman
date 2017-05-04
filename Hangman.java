e5ad2574f72fa39f4520560e9dc1c7fafefefefe
madhurimanohar <madhuri.manohar.r@gmail.com>
2017-05-04 10:07:50 -0700

    Update Hangman.java

--- Hangman.java
+++ Hangman.java
@@ -1,4 +1,18 @@
+import.java.util.Arrays;
+import.java.util.Random;
+import.java.util.Scanner;
 
 public class Hangman {
-
+  public static void main (String[] args) {
+    String list[] = {"math", "science", "english"};
+    Scanner s = new Scanner(System.in);
+    Random rand = new Random();
+    
+    int wrong = 0;
+    String word = (list[rand.nextInt(list.length)]);
+    boolean a = true;
+    
+    while(wrong != 10) {
+      
+                   
 }
