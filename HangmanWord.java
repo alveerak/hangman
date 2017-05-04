41022b26df398650f237231f5321dda8de84d37d
alveerak <alveerak@gmail.com>
2017-05-04 10:16:14 -0700

    Update HangmanWord.java

--- HangmanWord.java
+++ HangmanWord.java
@@ -1,4 +1,20 @@
 
 public class HangmanWord extends Word {
+	public boolean isComplete(){
+		for (int i = 0; i<letters.length; i++){
+			if (presentLetters[i]==false)
+				return false;
+		}
+		return true;
+	}
+	public boolean isLetter(char a){
+		boolean present = false;
+		for (int i = 0; i<letters.length; i++){
+			if (letters[i]==a){
+				present = true;
+				presentLetters[i]=true;
+			}
+		}
+	}
 
 }
