847a090947410821dd495528350e33e817a8c51b
alveerak <alveerak@gmail.com>
2017-05-04 10:09:14 -0700

    Update Word.java

--- Word.java
+++ Word.java
@@ -1,4 +1,16 @@
 
 public abstract class Word {
-
+	boolean [] presentLetters;
+	char [] letters;
+	public abstract boolean isComplete();
+	public abstract boolean isLetter();
+	public String getWord(){
+		String a = "";
+		for (int i = 0; i<letters.length; i++)
+			a+=letters[i];
+		return a;
+	}
+	public char getLetter(int index){
+		return letters[index];
+	}
 }
