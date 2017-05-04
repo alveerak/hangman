f5bda7a6521c64a6353d27342f1818e442c7756d
alveerak <alveerak@gmail.com>
2017-05-04 10:16:37 -0700

    Update Word.java

--- Word.java
+++ Word.java
@@ -1,9 +1,9 @@
 
 public abstract class Word {
-	boolean [] presentLetters;
-	char [] letters;
+	protected boolean [] presentLetters;
+	protected char [] letters;
 	public abstract boolean isComplete();
-	public abstract boolean isLetter();
+	public abstract boolean isLetter(char a);
 	public String getWord(){
 		String a = "";
 		for (int i = 0; i<letters.length; i++)
