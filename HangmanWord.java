7c645cede5b920e659d996109bad60294698405f
alveerak <alveerak@gmail.com>
2017-05-04 23:35:47 -0700

    Update HangmanWord.java

--- HangmanWord.java
+++ HangmanWord.java
@@ -1,5 +1,11 @@
-
 public class HangmanWord extends Word {
+	public HangmanWord(String a){
+		letters = new char [a.length()];
+		presentLetters = new boolean [a.length()];
+		for (int i = 0; i<a.length(); i++){
+			letters[i]=a.charAt(i);
+		}
+	}
 	public boolean isComplete(){
 		for (int i = 0; i<letters.length; i++){
 			if (presentLetters[i]==false)
@@ -7,14 +13,25 @@ public class HangmanWord extends Word {
 		}
 		return true;
 	}
-	public boolean isLetter(char a){
-		boolean present = false;
+	public void addLetter (char a){
 		for (int i = 0; i<letters.length; i++){
 			if (letters[i]==a){
-				present = true;
 				presentLetters[i]=true;
 			}
 		}
+	}
+	public boolean isLetter(int index){
+		return presentLetters[index];
+	}
+	public String makeLabel(){
+		String filling = "";
+		for (int i = 0; i<getWord().length(); i++){
+			if(presentLetters[i]==true)
+				filling+=letters[i]+" ";
+			else
+				filling+="_ ";
+		}
+		return filling;
 	}
 
 }
