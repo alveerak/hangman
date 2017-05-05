87b7b83ac7619e5ee24b0a66089beb6c25543e19
Labanya Mukhopadhyay <labanyamukhopadhyay@users.noreply.github.com>
2017-05-05 10:14:58 -0700

    Update Word.java

--- Word.java
+++ Word.java
@@ -1,14 +1,28 @@
+/**
+* The abstract Word class 
+*
+* @author 
+*/
 public abstract class Word {
 	protected boolean [] presentLetters;
 	protected char [] letters;
 	public abstract boolean isComplete();
 	public abstract void addLetter(char a);
+	/**
+	* This method returns the word from the letters 
+	* array of characters
+	*/
 	public String getWord(){
 		String a = "";
 		for (int i = 0; i<letters.length; i++)
 			a+=letters[i];
 		return a;
 	}
+	/**
+	* This method returns the letter at 
+	* a certain index
+	* @param index - the index of the desired letter
+	*/
 	public char getLetter(int index){
 		return letters[index];
 	}
