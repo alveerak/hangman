84d6562b6e61a22a27def1484f94ce22eafef711
alveerak <alveerak@gmail.com>
2017-05-06 21:00:57 -0700

    Update HangmanWord.java

--- HangmanWord.java
+++ HangmanWord.java
@@ -1,11 +1,4 @@
-/**
-* class extends the abstract class Word
-*/
 public class HangmanWord extends Word {
-	/**
-	* HangmanWord constructor adds letters of a given string to the letters array
-	* @param a - String of which's characters are added to letter array
-	*/
 	public HangmanWord(String a){
 		letters = new char [a.length()];
 		presentLetters = new boolean [a.length()];
@@ -13,9 +6,6 @@ public class HangmanWord extends Word {
 			letters[i]=a.charAt(i);
 		}
 	}
-	/**
-	* method 
-	*/
 	public boolean isComplete(){
 		for (int i = 0; i<letters.length; i++){
 			if (presentLetters[i]==false)
@@ -23,9 +13,6 @@ public class HangmanWord extends Word {
 		}
 		return true;
 	}
-	/**
-	*
-	*/
 	public void addLetter (char a){
 		for (int i = 0; i<letters.length; i++){
 			if (letters[i]==a){
@@ -33,15 +20,9 @@ public class HangmanWord extends Word {
 			}
 		}
 	}
-	/**
-	*
-	*/
 	public boolean isLetter(int index){
 		return presentLetters[index];
 	}
-	/**
-	*
-	*/
 	public String makeLabel(){
 		String filling = "";
 		for (int i = 0; i<getWord().length(); i++){
