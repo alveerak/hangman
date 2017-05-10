01d685d80d8cd9fda1c9e56fc041b82e304e7bbd
Labanya Mukhopadhyay <labanyamukhopadhyay@users.noreply.github.com>
2017-05-10 01:05:14 -0700

    Update HangmanFrame.java

--- HangmanFrame.java
+++ HangmanFrame.java
@@ -1,22 +1,29 @@
 import java.awt.*;
 import java.awt.event.*;
 import java.util.Random;
-
 import javax.swing.*;
 
+/**
+* class extends JFrame class and implements GUI 
+*/
 public class HangmanFrame extends JFrame{
 	HangmanDrawing a;
+	/** 
+	* constructor creates HangmanDrawing object
+	* 
+	*/
 	public HangmanFrame() {
 		a = new HangmanDrawing();
 		add(a);
-        
-        setResizable(true);
-        setTitle("Hangman");
-        setSize(800, 700);
-        setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-        setVisible(true);
+        	setResizable(true);
+        	setTitle("Hangman");
+        	setSize(800, 700);
+        	setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        	setVisible(true);
 	}
-
+	/**
+	* main method which creates object app of the class
+	*/
 	public static void main(String[] args) {
 		HangmanFrame app = new HangmanFrame();
 	}
