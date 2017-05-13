658f3e88db28f70df88ccc615212c9d8877f4021
kristielim <kristie.lim@gmail.com>
2017-05-13 01:10:03 -0700

    Update HangmanDrawing.java
    javadoc comments

--- HangmanDrawing.java
+++ HangmanDrawing.java
@@ -37,7 +37,8 @@ public class HangmanDrawing extends JPanel implements ActionListener {
 	private JButton btn; // submit button
 	private char l; // letter entered
 	private int moves; // number of incorrect moves done
-	private final int MAX_MOVES = 12;
+	private final int MAX_MOVES = 12; // max number of incorrect moves before
+										// player loses
 
 	// below are related to the list of words (spells)
 	private ArrayList<Word> list; // words randomly chosen for game
@@ -45,7 +46,8 @@ public class HangmanDrawing extends JPanel implements ActionListener {
 													// guess words (spells)
 
 	// count down variables
-	private final double MAX_SECS = 60000;
+	private final double MAX_SECS = 60000; // maximum time player has to win the
+											// game in milliseconds
 	private double remain = MAX_SECS; // how many sec left in count down.
 	private long update; // count last updated
 	private JLabel timeLabel; // displays count
@@ -53,18 +55,23 @@ public class HangmanDrawing extends JPanel implements ActionListener {
 	private NumberFormat format; // in seconds
 
 	private HangmanWord word; // word object version of hangman word
-	private boolean gameStarted; // so that nothing is drawn when the drawing
-									// object is initially created
-	private Random r = new Random();
+	private Random r = new Random(); // random object to choose a random game
+										// word
 
-	private JPanel submitPanel, wordPanel, textPanel, guessPanel, cluePanel;
-	private ImagePanel imagePanel;
+	private JPanel submitPanel, wordPanel, textPanel, guessPanel, cluePanel; // various
+																				// panels
+																				// of
+																				// game
+																				// screen
+	private ImagePanel imagePanel; // panel of game screen that contains
+									// "hangman" wizard image
 
 	/**
-	 * Constructor creates a Random object
+	 * Constructor to initialize all necessary instance variables: JFrame
+	 * panels, list of game words, this game word, all JFrame components, number
+	 * of moves, timer
 	 */
 	public HangmanDrawing() {
-		gameStarted = false;
 		moves = 0;
 
 		initializeLists();
@@ -74,7 +81,7 @@ public class HangmanDrawing extends JPanel implements ActionListener {
 		setVisible(true);
 		setBackground(new Color(225, 215, 178));
 		this.setSize(900, 800);
-		
+
 		guessedLetters = new JLabel("");
 		guessedLetters.setVisible(true);
 
@@ -92,7 +99,7 @@ public class HangmanDrawing extends JPanel implements ActionListener {
 		tf.setVisible(true);
 
 		btn = new JButton("Submit");
-		//btn.setLocation(125, 20);
+		// btn.setLocation(125, 20);
 		add(btn);
 		btn.addActionListener(this);
 
@@ -102,8 +109,8 @@ public class HangmanDrawing extends JPanel implements ActionListener {
 	}
 
 	/**
-	*
-	*/
+	 * when a button is clicked, perform corresponding action
+	 */
 	public void actionPerformed(ActionEvent evt) {
 		if (evt.getSource() == time) {
 			updateDisplay();
@@ -132,8 +139,7 @@ public class HangmanDrawing extends JPanel implements ActionListener {
 				if (wordString.indexOf(l) > -1) {
 					word.addLetter(l);
 					lbl.setText(word.makeLabel());
-					remain += 15000;
-					//imagePanel.repaint();
+					remain += 7000;
 				} else {
 					wrongGuess();
 				}
@@ -145,51 +151,49 @@ public class HangmanDrawing extends JPanel implements ActionListener {
 			} else if (moves >= MAX_MOVES) {
 				lose();
 			}
-			
+
 		}
 		imagePanel.repaint();
 	}
 
 	/**
 	 * returns the number of incorrect moves made so far
+	 * 
 	 * @return - the number of incorrect moves made so far
 	 */
 	public int getMoves() {
 		return moves;
 	}
 
-	
 	/**
 	 * Starts the timer
 	 */
-	public void start() { 
+	public void start() {
 		update = System.currentTimeMillis();
 		time.start();
-	} 
+	}
 
 	/**
 	 * Update the displayed time. This method is called from actionPerformed().
 	 */
 	public void updateDisplay() {
-		long now = System.currentTimeMillis(); //current time in ms
-		long elapsed = now - update; //ms elapsed since last update
-		remain -= elapsed; //adjust remaining time
-		update = now; //remember new update time
+		long now = System.currentTimeMillis(); // current time in ms
+		long elapsed = now - update; // ms elapsed since last update
+		remain -= elapsed; // adjust remaining time
+		update = now; // remember new update time
 
-		//convert remaining ms to ss format and display
-		int sec = (int)((remain) / 1000);
+		// convert remaining ms to ss format and display
+		int sec = (int) ((remain) / 1000);
 		timeLabel.setText(format.format(sec));
 
-		//stops the count down when it hits 0
+		// stops the count down when it hits 0
 		if (remain <= 0) {
 			lose();
 		}
 	}
-	
+
 	/**
-	 * Initializes list to spell names, initializes spellTypes and
-	 * spellDescriptions
-	 * 
+	 * Initializes list of Words to SpellWords and HangmanWords
 	 */
 	private void initializeLists() {
 		try {
@@ -215,6 +219,10 @@ public class HangmanDrawing extends JPanel implements ActionListener {
 		}
 	}
 
+	/**
+	 * private helper method to reset all necessary instance variables when user
+	 * elects to start a new game
+	 */
 	private void reset() {
 		word = (HangmanWord) list.get(r.nextInt(list.size()));
 		moves = 0;
@@ -227,11 +235,13 @@ public class HangmanDrawing extends JPanel implements ActionListener {
 		imagePanel.repaint();
 		btn.setText("Submit");
 		remain = MAX_SECS;
-		imagePanel.setMoves(0);
-		imagePanel.repaint();
 		this.start();
 	}
 
+	/**
+	 * private helper method to update game display when user makes an incorrect
+	 * guess
+	 */
 	private void wrongGuess() {
 		moves++;
 		if (tf.getText().length() == 1) {
@@ -243,6 +253,9 @@ public class HangmanDrawing extends JPanel implements ActionListener {
 			clue.setText("Hint: " + word.getHint());
 	}
 
+	/**
+	 * private helper method to update game display when user wins the game
+	 */
 	private void win() {
 		tf.setVisible(false);
 		imagePanel.repaint();
@@ -252,15 +265,22 @@ public class HangmanDrawing extends JPanel implements ActionListener {
 		time.stop();
 	}
 
+	/**
+	 * private helper method to update game display when user loses the game
+	 */
 	private void lose() {
 		tf.setVisible(false);
 		imagePanel.repaint();
 		lbl.setLocation(30, 100);
+		// lbl.setFont(new Font("Calibri", Font.BOLD, 30));
 		lbl.setText(word.getLostMessage());
 		btn.setText("New Game");
 		time.stop();
 	}
 
+	/**
+	 * private helper method to initialize the JPanels
+	 */
 	private void addPanels() {
 		submitPanel = new JPanel();
 		submitPanel.setBackground(new Color(225, 215, 178));
@@ -268,7 +288,6 @@ public class HangmanDrawing extends JPanel implements ActionListener {
 		submitPanel.setLayout(new FlowLayout());
 		submitPanel.add(tf);
 		submitPanel.add(btn);
-		// submitPanel.setBorder(BorderFactory.createLineBorder(Color.black));
 		add(submitPanel);
 
 		wordPanel = new JPanel();
@@ -277,7 +296,6 @@ public class HangmanDrawing extends JPanel implements ActionListener {
 		wordPanel.setBackground(new Color(225, 215, 178));
 		wordPanel.add(lbl);
 		wordPanel.setVisible(true);
-		// wordPanel.setBorder(BorderFactory.createLineBorder(Color.black));
 		add(wordPanel);
 
 		guessPanel = new JPanel();
@@ -286,7 +304,6 @@ public class HangmanDrawing extends JPanel implements ActionListener {
 		guessPanel.setBackground(new Color(225, 215, 178));
 		guessPanel.add(guessedLetters);
 		guessPanel.setVisible(true);
-		// guessPanel.setBorder(BorderFactory.createLineBorder(Color.black));
 		add(guessPanel);
 
 		cluePanel = new JPanel();
@@ -295,8 +312,6 @@ public class HangmanDrawing extends JPanel implements ActionListener {
 		cluePanel.setBackground(new Color(225, 215, 178));
 		cluePanel.add(clue);
 		cluePanel.setVisible(true);
-		// cluePanel.setBorder(BorderFactory.createLineBorder(Color.black));
-
 		add(cluePanel);
 
 		imagePanel = new ImagePanel();
@@ -304,10 +319,12 @@ public class HangmanDrawing extends JPanel implements ActionListener {
 		imagePanel.setBackground(new Color(225, 215, 178));
 		imagePanel.setPreferredSize(new Dimension(450, 300));
 		imagePanel.setVisible(true);
-		// imagePanel.setBorder(BorderFactory.createLineBorder(Color.black));
 		add(imagePanel);
 	}
 
+	/**
+	 * private helper method to initialize the timer
+	 */
 	private void time() {
 		// create JLabel to display remaining time
 		timeLabel = new JLabel();
