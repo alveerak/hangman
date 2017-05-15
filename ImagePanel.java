3f2da7da7efcaa004386081d2f6773fff5b0a298
kristielim <kristie.lim@gmail.com>
2017-05-14 21:15:47 -0700

    Update ImagePanel.java

--- ImagePanel.java
+++ ImagePanel.java
@@ -8,40 +8,69 @@ import java.util.Random;
 import javax.imageio.ImageIO;
 import javax.swing.JPanel;
 
-class ImagePanel extends JPanel{
-	private Image [] imageArray;
+/**
+ * class is the panel that displays the image of the evil wizard as player
+ * submits incorrect guesses
+ * 
+ */
+class ImagePanel extends JPanel {
+	private Image[] imageArray;
 	private int moves;
 	private int imageIndex;
-	public ImagePanel(){
+
+	/**
+	 * Constructor to initialize ImagePanel to be an image of a random evil
+	 * wizard
+	 */
+	public ImagePanel() {
 		loadImage();
 		Random r = new Random();
 		imageIndex = r.nextInt(3);
 	}
-	public void paintComponent(Graphics g){
+
+	/**
+	 * Draws portion of the image
+	 */
+	public void paintComponent(Graphics g) {
 		int x = 0;
 		int y = 0;
-		for (int i = 0; i<moves; i++){
-			g.drawImage(imageArray[imageIndex],x+200, y, x+300, y+100, x, y, x+100, y+100, null);
-			x+=100;
-			if(x==400){
-				y+=100;
-				x=0;
+		for (int i = 0; i < moves; i++) {
+			g.drawImage(imageArray[imageIndex], x + 200, y, x + 300, y + 100,
+					x, y, x + 100, y + 100, null);
+			x += 100;
+			if (x == 400) {
+				y += 100;
+				x = 0;
 			}
 		}
 	}
-	public void setMoves(int i){
+
+	/**
+	 * sets the number of moves so correct portion of image is displayed
+	 * 
+	 * @param i
+	 *            number of moves
+	 */
+	public void setMoves(int i) {
 		moves = i;
 	}
-	public void loadImage(){
-		imageArray = new Image [3];
-		try{
+
+	/**
+	 * reads in the images
+	 */
+	public void loadImage() {
+		imageArray = new Image[3];
+		try {
 			BufferedImage i = ImageIO.read(new File("draco.jpg"));
-			imageArray[0] = i.getScaledInstance(400, 300, BufferedImage.TYPE_INT_ARGB);
+			imageArray[0] = i.getScaledInstance(400, 300,
+					BufferedImage.TYPE_INT_ARGB);
 			i = ImageIO.read(new File("umbridge.jpg"));
-			imageArray[1] = i.getScaledInstance(400, 300, BufferedImage.TYPE_INT_ARGB);
+			imageArray[1] = i.getScaledInstance(400, 300,
+					BufferedImage.TYPE_INT_ARGB);
 			i = ImageIO.read(new File("voldemort.jpg"));
-			imageArray[2] = i.getScaledInstance(400, 300, BufferedImage.TYPE_INT_ARGB);
-		}catch(IOException e){
+			imageArray[2] = i.getScaledInstance(400, 300,
+					BufferedImage.TYPE_INT_ARGB);
+		} catch (IOException e) {
 			System.out.println("Problemo");
 		}
 	}
